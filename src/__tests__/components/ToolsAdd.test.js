c9dab90ae83a51f0da1cb17a5f4147f22e8d2fa1
santanarscs <raphaelstn@gmail.com>
2019-08-19 13:17:00 -0300

    update tests

--- src/__tests__/components/ToolsAdd.test.js
+++ src/__tests__/components/ToolsAdd.test.js
@@ -1,29 +1,26 @@
-import React from "react";
-import { useDispatch } from "react-redux";
-import { render, fireEvent } from "@testing-library/react";
-import { addToolRequest } from "~/store/modules/tools/actions";
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { render, fireEvent } from '@testing-library/react';
+import { addToolRequest } from '~/store/modules/tools/actions';
 
-import ToolsAdd from "~/components/ToolsAdd";
+import ToolsAdd from '~/components/ToolsAdd';
 
-jest.mock("react-redux");
+jest.mock('react-redux');
 
-describe("ToolsAdd Component", () => {
-  xit("should be able to add new tool", () => {
-    const { getByTestId, getByLabelText } = render(<ToolsAdd />);
+describe('ToolsAdd Component', () => {
+	it('should be able to add new tool', () => {
+		const dispatch = jest.fn();
+		useDispatch.mockReturnValue(dispatch);
 
-    const dispatch = jest.fn();
-    useDispatch.mockReturnValue(dispatch);
+		const { getByTestId, getByLabelText } = render(<ToolsAdd />);
+		fireEvent.change(getByLabelText('title'), {
+			target: { value: 'Title Tool' }
+		});
+		fireEvent.change(getByLabelText('description'), {
+			target: { value: 'Description Tool' }
+		});
+		fireEvent.submit(getByTestId('form'));
 
-    fireEvent.change(getByLabelText("title"), {
-      target: { value: "Title Tool" }
-    });
-    fireEvent.change(getByLabelText("description"), {
-      target: { value: "Description Tool" }
-    });
-    fireEvent.submit(getByTestId("form"));
-
-    expect(dispatch).toHaveBeenCalledWith(
-      addToolRequest({ title: "Title Tool", description: "Description Tool" })
-    );
-  });
+		expect(dispatch).toHaveBeenCalledWith(addToolRequest({ title: 'Title Tool', description: 'Description Tool' }));
+	});
 });
--- src/__tests__/components/ToolsItem.test.js
+++ src/__tests__/components/ToolsItem.test.js
@@ -1,29 +1,26 @@
-import React from "react";
-import { useDispatch } from "react-redux";
-import { render, fireEvent } from "@testing-library/react";
-import { removeToolRequest } from "~/store/modules/tools/actions";
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { render, fireEvent } from '@testing-library/react';
+import { removeToolRequest } from '~/store/modules/tools/actions';
 
-import ToolsItem from "~/components/ToolsItem";
+import ToolsItem from '~/components/ToolsItem';
 
-jest.mock("react-redux");
 const tool = {
-  id: 1,
-  title: "Notion",
-  link: "https://notion.so",
-  description:
-    "All in one tool to organize teams and ideas. Write, plan, collaborate, and get organized. ",
-  tags: ["organization", "planning", "collaboration", "writing", "calendar"]
+	id: 1,
+	title: 'Notion',
+	link: 'https://notion.so',
+	description: 'All in one tool to organize teams and ideas. Write, plan, collaborate, and get organized. ',
+	tags: ['organization', 'planning', 'collaboration', 'writing', 'calendar']
 };
+jest.mock('react-redux');
+describe('ToolsItem Component', () => {
+	it('should be able to remove tool', () => {
+		const dispatch = jest.fn();
+		useDispatch.mockReturnValue(dispatch);
 
-describe("ToolsItem Component", () => {
-  it("should be able to remove tool", () => {
-    const { getByTestId } = render(<ToolsItem tool={tool} />);
+		const { getByTestId } = render(<ToolsItem tool={tool} />);
+		fireEvent.click(getByTestId('remove-tool'));
 
-    const dispatch = jest.fn();
-    useDispatch.mockReturnValue(dispatch);
-
-    fireEvent.click(getByTestId("remove-tool"));
-
-    expect(dispatch).toHaveBeenCalledWith(removeToolRequest(tool.id));
-  });
+		expect(dispatch).toHaveBeenCalledWith(removeToolRequest(tool.id));
+	});
 });
